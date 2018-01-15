6a9b3eb0fc949e9582ed63fc3f084488ba20bc55
ngrieger <ngrieger@KO09C-PRAK26.scp.koeln.fh-aachen.de>
2018-01-15 14:50:35 +0100

    create wasweißich

---
+++ crypto_clicker/src/Game.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import {Button, Icon, Col, Input, Row} from 'react-materialize'
+import ReactDOM from 'react-dom';
+
+
+class Game extends Component {
+	constructor(props) {
+    super(props);
+    this.state = {email: ''};
+	this.state = {pw: ''};
+  }
+
+  render() {
+    return (
+	  <div>
+		
+	  </div>
+    );
+  }
+}
+
+
+
+export default Game;
