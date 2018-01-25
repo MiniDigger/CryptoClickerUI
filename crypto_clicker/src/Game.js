d639ad2e3f448ed2291c8458bb8d98300390d420
Amircal <nik-trash@gmx.de>
2018-01-25 11:31:02 +0100

    entwurf für rechte spalte

--- crypto_clicker/src/Game.js
+++ crypto_clicker/src/Game.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from './Bitcoin.svg';
+import gpu from './gpu.png';
 
-import {Button, Icon, Col, Input, Row, Tabs, Tab} from 'react-materialize'
+import {Button, Icon, Col, Input, Row, Tabs, Tab, Card, CardTitle} from 'react-materialize'
 import ReactDOM from 'react-dom';
 
 
@@ -23,7 +24,8 @@ class Game extends Component {
 	  var imgStyle={
 		margin: 'auto', 
 		position: 'absolute', 
-		top: 0, left: 0, right: 0, bottom: 0
+		top: 0, left: 0, right: 0, bottom: 0,
+		height: '200px'
 	  };
 	  var tabsStyle={
 		color: 'black',
@@ -33,46 +35,64 @@ class Game extends Component {
 	  <div>
 		<Row>
 			<Col s={3} style={colStyle}>
-			<div style={divStyle}>
-					<div class="col s12" style={{padding: "0px"}}>
-						<ul class="tabs z-depth-1 black" style={{display: "inline-grid"}}>
-							<li class="tab col" style={{margin: "auto"}}>
-								<a class="active" href="#crypto" style={{color:"white"}}></a>
-							</li>
-							<div class="indicator" style={{right: "279px", left: "0px", backgroundColor:"black"}}></div>
-						</ul>
-					</div>
-					<div style={divStyle} id="crypto">
+				<div className="col s12" style={{padding: "0px"}}>
+					<ul className="tabs z-depth-1 black" style={{display: "inline-grid"}}>
+						<li className="tab col" style={{margin: "auto"}}>
+							<a className="active" href="#crypto" style={{color:"white"}}></a>
+						</li>
+						<div className="indicator" style={{right: "279px", left: "0px", backgroundColor:"black"}}></div>
+					</ul>
+				</div>
+				<div style={divStyle} id="crypto">
 					<img style={imgStyle} src={logo} className="App-logo" alt="logo"/>
 				</div>
-			</div>
-				
 			</Col>
 			<Col s={6} style={colStyle}>
-				<div style={divStyle}>
-					<div class="col s12" style={{padding: "0px"}}>
-						<ul class="tabs z-depth-1 black" style={{display: "inline-grid"}}>
-							<li class="tab col" style={{margin: "auto"}}>
-								<a class="active" style={{color:"white"}}>Highscore</a>
-							</li>
-							<div class="indicator" style={{right: "279px", left: "0px", backgroundColor:"white"}}></div>
-						</ul>
-					</div>
+				<div className="col s12" style={{padding: "0px"}}>
+					<ul className="tabs z-depth-1 black" style={{display: "inline-grid"}}>
+						<li className="tab col" style={{margin: "auto"}}>
+							<a className="active" style={{color:"white"}}>Highscore</a>
+						</li>
+						<div className="indicator" style={{right: "279px", left: "0px", backgroundColor:"white"}}></div>
+					</ul>
+				</div>
+				<div style={divStyle} id="tab_00">
+					
 				</div>
 			</Col>
 			<Col s={3} style ={{padding: "0px"}}>
-				<div style={divStyle}>
-					<div class="col s12" style={{padding: "0px"}}>
-						<ul class="tabs z-depth-1 black">
-							<li class="tab col">
-								<a href="#tab_00" class="active" style={{color:"white"}}>Generatoren</a>
-							</li>
-							<li class="tab col">
-								<a href="#tab_01" class="" style={{color:"white"}}>Shop</a>
-							</li>
-							<div class="indicator" style={{right: "279px", left: "0px", backgroundColor:"white"}}></div>
-						</ul>
-					</div>
+				<div className="col s12" style={{padding: "0px"}}>
+					<ul className="tabs z-depth-1 black">
+						<li className="tab col">
+							<a href="#generatoren" className="active" style={{color:"white"}}>Generatoren</a>
+						</li>
+						<li className="tab col">
+							<a href="#upgrades" className="" style={{color:"white"}}>Upgrades</a>
+						</li>
+						<div className="indicator" style={{right: "279px", left: "0px", backgroundColor:"white"}}></div>
+					</ul>
+				</div>
+				<div id="generatoren">
+					<Col m={12}>
+						<Card className='small horizontal' header={<CardTitle key='1' image={gpu} src={gpu} style={{padding:'10px'}}/>} actions={[<Button key='1' floating large className='red' waves='light' icon='add'/>]}>
+							I am a very simple card.
+						</Card>
+						<Card className='small horizontal' header={<CardTitle key='1' image={gpu} src={gpu} style={{padding:'10px'}}/>} actions={[<Button key='1' floating large className='red' waves='light' icon='add'/>]}>
+							I am a very simple card.
+						</Card>
+						<Card className='small horizontal' header={<CardTitle key='1' image={gpu} src={gpu} style={{padding:'10px'}}/>} actions={[<Button key='1' floating large className='red' waves='light' icon='add'/>]}>
+							I am a very simple card.
+						</Card>
+						<Card className='small horizontal' header={<CardTitle key='1' image={gpu} src={gpu} style={{padding:'10px'}}/>} actions={[<Button key='1' floating large className='red' waves='light' icon='add'/>]}>
+							I am a very simple card.
+						</Card>
+						<Card className='small horizontal' header={<CardTitle key='1' image={gpu} src={gpu} style={{padding:'10px'}}/>} actions={[<Button key='1' floating large className='red' waves='light' icon='add'/>]}>
+							I am a very simple card.
+						</Card>
+					</Col>
+				</div>
+				<div id="upgrades">
+					<h1>test</h1>
 				</div>
 			</Col>
 		</Row>
